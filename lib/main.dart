41808da8ff304724cab43690e140c97879d25a7c
FlutLab.io <>
2022-01-31 09:27:24 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(backgroundColor: Colors.blueGrey[900], title: Text("I Am Rich")),
+        body: SafeArea(
+          child: Center(
+            child: Image(image: AssetImage('assets/images/diamond.png')),
+          ),
+        ),
+      ),
+    );
+  }
+}
